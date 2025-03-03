bffd433307ae4e705c87d9abaa31e50e03a23fb0
ivonnenabangala <ivonnenabangala@gmail.com>
2025-03-03 16:14:08 +0300

    delete document

--- citizen-connect-fe/src/app/admin/admin-documents/admin-documents.component.ts
+++ citizen-connect-fe/src/app/admin/admin-documents/admin-documents.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Documents } from '../../interfaces/documents';
 import { DocumentsService } from '../../services/documents.service';
 import { ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatListModule} from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-admin-documents',
@@ -13,11 +14,14 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './admin-documents.component.scss'
 })
 export class AdminDocumentsComponent {
-documents:Documents[] = []
+  documents:Documents[] = []
+  errorMessage:string = ''
+  successMessage:string = ''
 
   constructor(
     private documentsService: DocumentsService,
-    private cdr: ChangeDetectorRef
+    private cdr: ChangeDetectorRef,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
@@ -39,4 +43,37 @@ documents:Documents[] = []
         }
       }) 
     }
+
+    deleteDocument(document: Documents): void {
+      this.successMessage = '';
+      this.errorMessage = '';
+      const horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+      const verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+    
+      const snackBarRef = this.snackBar.open(
+        'Are you sure you want to delete this document?', 
+        'Yes', 
+        { duration: 5000, horizontalPosition, verticalPosition } // Auto-close after 5 seconds
+      );
+    
+      snackBarRef.onAction().subscribe(() => {
+        this.documentsService.delete(document).subscribe({
+          next: () => {
+            console.log(`Document ${document.documentId} deleted successfully`);
+            this.documents = this.documents.filter(i => i.documentId !== document.documentId); 
+            this.cdr.detectChanges();
+            this.snackBar.open('Document deleted successfully!', 'Close', { 
+              duration: 3000, horizontalPosition, verticalPosition });
+          },
+          error: (err) => {
+            this.errorMessage = err?.error?.message || 'An error occurred while deleting the document.';
+            console.error('Delete failed:', err);
+            this.snackBar.open(this.errorMessage, 'Close', { 
+              duration: 3000, horizontalPosition, verticalPosition });
+          } 
+        });
+      });
+    }
+    
 }
